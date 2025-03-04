f729042190322e70a88a2ce38c15c8b3e873df56
mausoleo <leomelo35@hotmail.com>
2025-03-04 00:28:23 -0300

    finAll implementation

--- src/model/application/Main.java
+++ src/model/application/Main.java
@@ -21,5 +21,11 @@ public class Main {
         for (Seller seller1 : list) {
             System.out.println(seller1);
         }
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        for (Seller seller1 : list) {
+            System.out.println(seller1);
+        }
     }
 }
