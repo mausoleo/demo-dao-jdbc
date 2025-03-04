a24a5e91ae47e4524861c62ae973283705cd40f1
mausoleo <leomelo35@hotmail.com>
2025-03-03 21:56:21 -0300

    findByYd implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DatabaseConnection;
 import model.dao.imple.SellerDaoImpleJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoImpleJDBC();
+        return new SellerDaoImpleJDBC(DatabaseConnection.getConnection());
     }
 }
